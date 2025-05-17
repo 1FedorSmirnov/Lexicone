5895b4436f1ae9ed95294c15c3144c0c85c2b670
Fedor <smirnov.fe@gmail.com>
2025-05-17 16:04:07 +0300

    3 Control point

--- src/main/java/com/example/lexicone/model/WordStatus.java
+++
@@ -1,7 +0,0 @@
-package com.example.lexicone.model;
-
-public enum WordStatus {
-    NEW,
-    IS_LEARNING,
-    LEARNT
-}
---
+++ training-system/src/main/java/com/example/trainingsystem/model/User.java
@@ -0,0 +1,60 @@
+package com.example.trainingsystem.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "role", nullable = false)
+    private String role;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
